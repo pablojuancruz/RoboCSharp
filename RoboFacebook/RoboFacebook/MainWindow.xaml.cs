b7f1e7ae39fc7565337fca44263c3011947b7cdf
unknown <pablojuan@foryoursystem.com>
2015-05-21 22:47:08 -0300

    first commit

---
+++ RoboFacebook/RoboFacebook/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace RoboFacebook
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviarMensagem_Click(object sender, RoutedEventArgs e)
+        {
+            string robo = @"C:\RoboFace.rob";
+            ProcessStartInfo startInfo = new ProcessStartInfo("TASKEXEC.EXE");
+
+            startInfo.Arguments = String.Format(" \"{0}\"  \"{1}\" \"{2}\" \"{3}\"", robo, txtEmail.Text, txtSenha.Password, txtMensagem.Text);
+            Process.Start(startInfo);    
+        }
+    }
+}
+
